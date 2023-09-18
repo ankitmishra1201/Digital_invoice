9efa35c2723895431e7530d3b37c476f5bbb1c64
Aman <vaman971@gmail.com>
2023-09-19 04:39:58 +0530

    changes made to backend api

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
 const connectDB = require("./config/database");
 const productRoutes = require('./routes/productRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
 const cors = require('cors');
-const path=require('path')
-const app = express();
+const path=require('path');
 
 
 app.use(cors());
-app.use(express.static(path.join(__dirname,"client/build")))
+app.use(express.static(path.join(__dirname,"../client/build")))
+
 dotenv.config();
 
 connectDB(); // for connecting with our database
@@ -17,12 +18,14 @@ connectDB(); // for connecting with our database
 app.use(express.json());
 
 app.get('/', (req,res)=>{
+    // res.sendFile(path.join(__dirname,"client/build/index.html"))
     res.json({message: "API running..."});
 })
 
 // Routes
 app.use('/api/products',productRoutes);
 app.use('/api/services',serviceRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 try {
