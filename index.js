f6b7a19999a6465b4db5efec0b7cb9f75238ead4
Aman <vaman971@gmail.com>
2023-09-19 12:29:09 +0530

    debugging done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path=require('path');
 
 
 app.use(cors());
-app.use(express.static(path.join(__dirname,"../client/build")))
+app.use(express.static(path.join(__dirname,"/client/build")))
 
 dotenv.config();
 
